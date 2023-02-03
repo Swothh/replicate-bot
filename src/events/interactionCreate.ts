2021b945ffec0318159ccbf9310035f41c9b42de
Swôth <56405862+Swothh@users.noreply.github.com>
2023-02-03 17:30:40 +0300

    ❌ cancel button added

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -6,6 +6,13 @@ export const Event: IEvent = {
     name: Events.InteractionCreate,
     type: 'on',
     run: async (client, interaction: Interaction) => {
+        if (interaction.isButton() && interaction.customId === 'cancel_job') {
+            const job = client.jobs.get(interaction.user.id);
+            if (!job) return;
+            job.interaction.deleteReply();
+            return client.jobs.delete(interaction.user.id);
+        };
+
         if (!interaction.isCommand() || !interaction.isChatInputCommand()) return;
         const command = client.commands.get(interaction.commandName);
         if (!command) return;
